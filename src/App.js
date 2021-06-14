016e0d0bfa9c5a62a906e7a33af8214e9447671e
Nobuuuuuu <lifendyel@gmail.com>
2021-06-14 19:57:57 +0200

    :sparkles: Ajout header/menu SearchBar du site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Header from './Header'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+      </div>
+    </Router>
   );
 }
 
