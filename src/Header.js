5679cee32287e066d98e7430b4e5182fe1b6f41f
Nobuuuuuu <lifendyel@gmail.com>
2021-06-14 20:18:50 +0200

    :sparkles: Ajout autre partie du header et début Login

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 import './Header.css';
 import { Link } from 'react-router-dom'
 
@@ -11,10 +12,35 @@ function Header() {
                 <input type="text" className="header__searchInput" />
                 <SearchIcon className="header__searchIcon" />
             </div>
-            <Link to="/" className="header__link">
-                <div className="header__option">
-                    <span className="header__optionLineOne">Hello, User</span>
-                    <span className="header__optionLineTwo">Sign in or Sign out</span>
+            <div className="header__nav">
+                {/* 1st link */}
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Hello, User</span>
+                        <span className="header__optionLineTwo">Sign in or Sign out</span>
+                    </div>
+                </Link>
+                {/* 2st link */}
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo">& Orders</span>
+                    </div>
+                </Link>
+                {/* 3st link */}
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo">Prime</span>
+                    </div>
+                </Link>
+            </div>
+            {/* basket icon with number */}
+            <Link to="/checkout" className="header__link">
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon/>
+                    {/* number of items in the basket */}
+                    <span className="header__optionLineTwo header__basketCount">2</span>
                 </div>
             </Link>
         </nav>
