8e39ac13395795bc27c4476f797ed50c7a6e2f79
guidezSeb <sebastien.guidez@lacatholille.fr>
2021-06-15 01:49:00 +0200

    product connect database

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import './Home.css';
 import Product from './Product'
+import {db} from './firebase'
+import { red } from '@material-ui/core/colors';
 
 function Home(){
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const ref = db.collection("products");
+    
+function getProducts() {
+    setLoading(true);
+    ref.onSnapshot((querySnapshot) => {
+        const items = [];
+        querySnapshot.forEach((doc) => {
+            items.push(doc.data());
+        });
+        setProducts(items);
+        setLoading(false);
+    });
+}
+
+    useEffect(()=> {
+        getProducts();
+    }, []);
+
+    if (loading) {
+        return <h2>loading...</h2>;
+    }
     return (
         <div className="home">
             <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/08/em/pd21/xcm_em_Prime_Day_2021_856-FRFR_D_PDS-HP-Tall-Hero_1500x600._CB667248309_.jpg" alt=""/>
+          
+            
             <div className="home__row">
-                <Product id="1231223" title="The Lean Startup: How Contant Innovation" price={11.96} rating={4} image="https://images-eu.ssl-images-amazon.com/images/I/41uNAbThwiL._SY291_BO1,204,203,200_QL40_ML2_.jpg"/>
-                <Product id="1231223" title="The Lean Startup: How Contant Innovation" price={12.96} rating={3} image="https://images-eu.ssl-images-amazon.com/images/I/41uNAbThwiL._SY291_BO1,204,203,200_QL40_ML2_.jpg"/>
-                <Product id="1231223" title="The Lean Startup: How Contant Innovation" price={13.96} rating={2} image="https://images-eu.ssl-images-amazon.com/images/I/41uNAbThwiL._SY291_BO1,204,203,200_QL40_ML2_.jpg"/>
+            {products.map((product) => (
+                <Product id={product.id} title={product.title} price={product.price} rating={product.rating} image={product.image}/>
+                ))}
             </div>
-            <div className="home__row">
-                <Product id="1231223" title="The Lean Startup: How Contant Innovation" price={11.96} rating={4} image="https://images-eu.ssl-images-amazon.com/images/I/41uNAbThwiL._SY291_BO1,204,203,200_QL40_ML2_.jpg"/>
-                <Product id="1231223" title="The Lean Startup: How Contant Innovation" price={12.96} rating={3} image="https://images-eu.ssl-images-amazon.com/images/I/41uNAbThwiL._SY291_BO1,204,203,200_QL40_ML2_.jpg"/>
-                <Product id="1231223" title="The Lean Startup: How Contant Innovation" price={13.96} rating={2} image="https://images-eu.ssl-images-amazon.com/images/I/41uNAbThwiL._SY291_BO1,204,203,200_QL40_ML2_.jpg"/>
-                <Product id="1231223" title="The Lean Startup: How Contant Innovation" price={13.96} rating={2} image="https://images-eu.ssl-images-amazon.com/images/I/41uNAbThwiL._SY291_BO1,204,203,200_QL40_ML2_.jpg"/>
-            </div>
-            <div className="home__row">
-                <Product id="1231223" title="The Lean Startup: How Contant Innovation" price={11.96} rating={4} image="https://images-eu.ssl-images-amazon.com/images/I/41uNAbThwiL._SY291_BO1,204,203,200_QL40_ML2_.jpg"/>
-                <Product id="1231223" title="The Lean Startup: How Contant Innovation" price={12.96} rating={3} image="https://images-eu.ssl-images-amazon.com/images/I/41uNAbThwiL._SY291_BO1,204,203,200_QL40_ML2_.jpg"/>
-            </div>
+            
+                
+           
+            
         </div>
     )
 }
