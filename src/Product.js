b4b3bf3dc73a672429188ad0ea7b34cb895a0101
Nobuuuuuu <lifendyel@gmail.com>
2021-06-14 22:14:03 +0200

    :sparkles: Ajout du basket

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import './Product.css';
+import {useStateValue} from './StateProvider'
 
-function Product({id, title, image, price, rating}){
-    return(
+function Product({ id, title, image, price, rating }) {
+    const [{basket}, dispatch] = useStateValue();
+
+    console.log('basket content', basket)
+
+    const addToBasket = () => {
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        })
+    }
+    return (
         <div className="product">
             <div className="product__info">
                 <p>{title}</p>
@@ -10,16 +27,16 @@ function Product({id, title, image, price, rating}){
                 <div className="product__rating">
                     {
                         Array(rating)
-                        .fill()
-                        .map((_)=> (
-                            <p>*</p>
-                        ))
+                            .fill()
+                            .map((_) => (
+                                <p>*</p>
+                            ))
                     }
                 </div>
-                
+
             </div>
-            <img src={image} alt=""/>
-            <button>Add to Basket</button>
+            <img src={image} alt="" />
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
